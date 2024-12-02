cfbbeb20fdd8a2943a52d974e8c20733edf793c4
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-12-02 10:06:13 +0900

    tes

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -12,7 +12,7 @@ class TestModule(unittest.TestCase):
     self.assertEqual(add_numbers(2, 2), 5)
   
   def test_login(self):
-    self.assertEqual(login(), 2)
+    self.assertEqual(login(),2)
 
 
 if __name__ == '__main__':
